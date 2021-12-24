e139e7ec09aedca57129f05190b9eb0f16a57fb2
ilyass-elfouih <ilyass.elfouih@gmail.com>
2021-12-25 00:41:33 +0100

    chore: adding mocks and city model

---
+++ apps/weather-forecast/src/app/core/mocks/cities.mocks.ts
@@ -0,0 +1,25 @@
+import { CitiesModel } from "../models";
+
+export const CITIES_MOCKS:CitiesModel[]=[
+	{
+		country:"MA",
+		lat:2622,
+		lon:666,
+		local_names:{en:"Morocco"},
+		name:"Casablanca"
+	},
+	{
+		country:"MA",
+		lat:2622,
+		lon:666,
+		local_names:{en:"Morocco"},
+		name:"Agadir"
+	},
+	{
+		country:"MA",
+		lat:2622,
+		lon:666,
+		local_names:{en:"Morocco"},
+		name:"Marrakech"
+	},
+]
