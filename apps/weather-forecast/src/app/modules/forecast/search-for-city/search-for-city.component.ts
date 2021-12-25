08a4e235796aea48403265e7fc301661968a74a2
ilyass-elfouih <ilyass.elfouih@gmail.com>
2021-12-25 11:57:07 +0100

    feat: adding search city router logic & some UI

--- apps/weather-forecast/src/app/modules/forecast/search-for-city/search-for-city.component.ts
+++ apps/weather-forecast/src/app/modules/forecast/search-for-city/search-for-city.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
-import { CITIES_MOCKS } from '@weather-forcast/core/mocks/cities.mocks';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+//import { CITIES_MOCKS } from '@weather-forcast/core/mocks/cities.mocks';
 import { CitiesModel } from '@weather-forcast/core/models';
-import { Observable, of } from 'rxjs';
+import { distinctUntilChanged, filter, map, Observable, of, tap } from 'rxjs';
 
 @Component({
 	selector: 'bp-search-for-city',
 	templateUrl: './search-for-city.component.html',
 	styleUrls: ['./search-for-city.component.css']
 })
-export class SearchForCityComponent  {
+export class SearchForCityComponent implements OnInit {
 
-	cities$:Observable<CitiesModel[]>=of(CITIES_MOCKS)
+	cities$:Observable<CitiesModel[]>=of([])
+	searchText$:Observable<string>
+	constructor(
+		private activatedRoute:ActivatedRoute,
+		private router:Router
+	){}
+
+
+	ngOnInit(): void {
+		this.searchText$=this.activatedRoute
+			.queryParams
+			.pipe(
+				map(item=>item.cityName),
+				filter(item=>item),
+				distinctUntilChanged(),
+				tap(text=>{
+				//TODO: call Reducer here 
+				})
+			)
+	}
+
+	onSearchCityChanged(cityName:string){
+		this.router.navigate([],{queryParams:{cityName}})
+	}
+
+	
 }
--- apps/weather-forecast/src/app/shared/forecast-type-ahead/forecast-type-ahead/forecast-type-ahead.component.ts
+++ apps/weather-forecast/src/app/shared/forecast-type-ahead/forecast-type-ahead/forecast-type-ahead.component.ts
@@ -5,28 +5,23 @@ import { debounceTime, distinctUntilChanged, map, Observable, OperatorFunction, 
 @Component({
 	selector: 'bp-forecast-type-ahead-ui',
 	templateUrl: './forecast-type-ahead.component.html',
-	styleUrls: ['./forecast-type-ahead.component.css']
+	styleUrls: ['./forecast-type-ahead.component.css'],
 })
 export class ForecastTypeAheadComponent {
+	@Input() label: string;
+	@Input() placeholder: string;
+	@Input() foundCities$: Observable<CitiesModel[]>;
+	@Input() isLoading: boolean;
+	@Input() searchText = '';
+	@Output() searchTextChnage: EventEmitter<string> = new EventEmitter();
 
-
-	@Input() label:string
-	@Input() placeholder:string
-	@Input() foundCities$:Observable<CitiesModel[]>
-	@Input() isLoading:boolean
-	@Input() searchText="" 
-	@Output() searchTextChnage:EventEmitter<string>=new EventEmitter()
-
-	onSearchForCity:OperatorFunction<string,string[]>=(text$:Observable<string>)=>
+	onSearchForCity: OperatorFunction<string, string[]> = (text$: Observable<string>) =>
 		text$.pipe(
 			debounceTime(500),
 			distinctUntilChanged(),
-			tap((term)=>this.searchTextChnage.emit(term)),
-			switchMap(term=>this.foundCities$
-				.pipe(
-					map(cities=>cities.map(item=>`${item.name}(${item.local_names?.en})`))
-				)
+			tap(term => this.searchTextChnage.emit(term)),
+			switchMap(term =>
+				this.foundCities$.pipe(map(cities => cities.map(item => `${item.name}(${item.local_names?.en})`)))
 			)
-		)
- 
+		);
 }
