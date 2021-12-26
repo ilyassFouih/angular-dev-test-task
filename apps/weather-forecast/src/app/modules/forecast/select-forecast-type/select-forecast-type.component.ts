8c96d615903d0def9834a099edf4984f3d2d0e8b
ilyass-elfouih <ilyass.elfouih@gmail.com>
2021-12-26 23:24:00 +0100

    fix: showing selected forecast type

--- apps/weather-forecast/src/app/modules/forecast/select-forecast-type/select-forecast-type.component.ts
+++ apps/weather-forecast/src/app/modules/forecast/select-forecast-type/select-forecast-type.component.ts
@@ -1,20 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {  Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
 	selector: 'bp-select-forecast-type',
 	templateUrl: './select-forecast-type.component.html',
-	styleUrls: ['./select-forecast-type.component.css'],
-	changeDetection:ChangeDetectionStrategy.Default
+	styleUrls: ['./select-forecast-type.component.css']
 })
-export class SelectForecastTypeComponent {
+export class SelectForecastTypeComponent implements OnInit {
 
 	selected:string
 
 	constructor(
-		private router:Router
+		private router:Router,
+		private activatedRoute:ActivatedRoute
 	){
 		
+	}
+	ngOnInit(): void {
+		this.selected=this.activatedRoute.snapshot.queryParams.forecastType
 	}
 
 	onNewTypeSelected(forecastType:string){
