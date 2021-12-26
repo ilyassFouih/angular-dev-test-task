95b103ee2e766c94f7a5472baaa12887300cb54e
ilyass-elfouih <ilyass.elfouih@gmail.com>
2021-12-26 20:15:28 +0100

    feat: saving forcastType to route

--- apps/weather-forecast/src/app/modules/forecast/search-for-city/search-for-city.component.ts
+++ apps/weather-forecast/src/app/modules/forecast/search-for-city/search-for-city.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {  Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { CitiesModel } from '@weather-forcast/core/models';
-import { map, Observable, tap } from 'rxjs';
+import { distinctUntilChanged, map, Observable, tap } from 'rxjs';
 import { searchForCity, noCityFound } from '@weather-forcast/state/cities/city.actions';
 import { State } from '@weather-forcast/state/state';
 import { isLoadingCities, noCityFoundSelector, selectFoundCities } from '@weather-forcast/state/cities/city.selectors';
@@ -10,7 +10,7 @@ import { isLoadingCities, noCityFoundSelector, selectFoundCities } from '@weathe
 @Component({
 	selector: 'bp-search-for-city',
 	templateUrl: './search-for-city.component.html',
-	styleUrls: ['./search-for-city.component.css'],
+	styleUrls: ['./search-for-city.component.css']
 })
 export class SearchForCityComponent implements OnInit {
 	city$: Observable<CitiesModel>=this.store.select(selectFoundCities)
@@ -27,6 +27,7 @@ export class SearchForCityComponent implements OnInit {
 	ngOnInit(): void {
 		this.searchText$ = this.activatedRoute.queryParams.pipe(
 			map(item => item.cityName),
+			distinctUntilChanged(),
 			tap(cityName => {
 				const dispatchSearchOrnew=cityName?searchForCity({ cityName }):noCityFound()
 				this.store.dispatch(dispatchSearchOrnew);
--- apps/weather-forecast/src/app/modules/forecast/select-forecast-type/select-forecast-type.component.ts
+++ apps/weather-forecast/src/app/modules/forecast/select-forecast-type/select-forecast-type.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'bp-select-forecast-type',
 	templateUrl: './select-forecast-type.component.html',
-	styleUrls: ['./select-forecast-type.component.css']
+	styleUrls: ['./select-forecast-type.component.css'],
+	changeDetection:ChangeDetectionStrategy.Default
 })
 export class SelectForecastTypeComponent {
 
 	selected:string
+
+	constructor(
+		private router:Router
+	){
+
+	}
+
+	onNewTypeSelected(forecastType:string){
+		this.router.navigate([],{queryParams:{forecastType},queryParamsHandling:"merge"})
+	}	
 }
