5a07507098f6da9aa815f6ae8105e636501200ee
ilyass-elfouih <ilyass.elfouih@gmail.com>
2021-12-27 01:06:08 +0100

    feat: adding listen to city & type then fetch

--- apps/weather-forecast/src/app/modules/forecast/weather-forecast/weather-forecast.component.ts
+++ apps/weather-forecast/src/app/modules/forecast/weather-forecast/weather-forecast.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { FORECAST_TYPE } from '@weather-forcast/core/models';
+import { selectFoundCities } from '@weather-forcast/state/cities/city.selectors';
+import { LoadForecastAction } from '@weather-forcast/state/forecast-weather/forecast-weather.actions';
+import { State } from '@weather-forcast/state/state';
+import { combineLatest, filter, map, Subscription } from 'rxjs';
 @Component({
 	selector: 'bp-weather-forecast',
 	templateUrl: './weather-forecast.component.html',
-	styleUrls: ['./weather-forecast.component.css']
+	styleUrls: ['./weather-forecast.component.css'],
 })
-export class WeatherForecastComponent{
-
-	selected:string=null
-	
+export class WeatherForecastComponent implements OnInit, OnDestroy {
+	subscriptions: Subscription = new Subscription();
+	constructor(private store: Store<State>, private activatedRoute: ActivatedRoute) {}
+	ngOnDestroy(): void {
+		this.subscriptions.unsubscribe();
+	}
+	ngOnInit() {
+		const forecastType$ = this.activatedRoute.queryParams.pipe(map(q => q.forecastType as FORECAST_TYPE));
+		const foundCity$ = this.store.select(selectFoundCities);
+		const sub = combineLatest([forecastType$, foundCity$])
+			.pipe(
+				filter(([type, city]) => !!type && !!city),
+				map(([type, city]) => ({ lat: city.lat, lon: city.lon, typeWeather: type }))
+			)
+			.subscribe(dto => this.store.dispatch(LoadForecastAction(dto)));
+		this.subscriptions.add(sub);
+	}
 }
